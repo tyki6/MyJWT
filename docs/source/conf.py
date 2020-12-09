3a5bb9767c357ea0d96af2e470c1ef06fa00d64f
matthieu Bouamama <matthieu.bouamama@leroymerlin.fr>
2020-12-09 14:00:08 +0100

    change version -> 1.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, matthieu Bouamama"
 author = "matthieu Bouamama"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.3"
+release = "1.2.0"
 
 
 # -- General configuration ---------------------------------------------------
