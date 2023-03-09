b5bf41a126becf5006be5312f1c56493d6cc8e96
tyki6 <matthieubouamama@gmail.com>
2023-03-09 17:23:45 +0100

    fix: poetry python version for unit test

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
